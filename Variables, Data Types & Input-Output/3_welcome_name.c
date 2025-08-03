6afa76f7efa14be677520d7ca699fd8bdddbf796
Saad Ahmed <saadahmd7223@gmail.com>
2025-08-03 16:30:02 +0530

    3/100

--- Variables, Data Types & Input-Output/3_welcome_name.c
+++ Variables, Data Types & Input-Output/3_welcome_name.c
@@ -1,7 +1,8 @@
 // Create a program to input name of the person and respond with "Welcome to 100 C programming challenges"
 #include <stdio.h>
 
-int main(){
+int main()
+{
     char name[50];
     
     printf("Please enter your name: ");
