5fc3c08d064e96b31a76b2f54971f26512fb7e13
Saad Ahmed <saadahmd7223@gmail.com>
2025-08-03 16:54:32 +0530

    8/100

---
+++ Variables, Data Types & Input-Output/8_circumference_of_circle.c
@@ -0,0 +1,12 @@
+// Program to define a constant for the mathematical value pi (3.14159) and use it to calculate and print the circumference of a circle with a radius input from user.
+#include <stdio.h>
+
+int main(){
+    const float PI = 3.14159;
+    int radius;
+    printf("Enter the radius of the circle in cms: ");
+    scanf("%d",&radius);
+
+    printf("The circumference of your circle is %f cms\n", 2*PI*radius);
+    return 0;
+}
