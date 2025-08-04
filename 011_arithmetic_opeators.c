dad17f8fd6a8fcaaa86d2a2190fcad468fbcf15a
Saad Ahmed <saadahmd7223@gmail.com>
2025-08-04 23:57:32 +0530

    11/100

---
+++ 011_arithmetic_opeators.c
@@ -0,0 +1,20 @@
+// Create a program that takes two numbers and shows result of all arithmetic operators (+,-,*,/,%).
+#include <stdio.h>
+
+int main(){
+    int a,b;
+    printf("Enter the first number: ");
+    scanf("%d",&a);
+    printf("Enter the second number: ");
+    scanf("%d",&b);
+
+    printf("Here are the results of the operations:\n");
+    printf("%d + %d = %d\n", a, b, a+b);
+    printf("%d - %d = %d\n", a, b, a-b);
+    printf("%d * %d = %d\n", a, b, a*b);
+    printf("%d / %d = %d\n", a, b, a/b);
+    printf("%d % %d = %d\n", a, b, a%b);
+
+
+    return 0;
+}
