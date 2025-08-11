9d0ec12043d84dc632dd38d960e831f459c660d0
Saad Ahmed <saadahmd7223@gmail.com>
2025-08-11 23:33:00 +0530

    26/100

--- 026_odd_even_ternary.c
+++ 026_odd_even_ternary.c
@@ -1,1 +1,11 @@
 // Create a program to find if the give number is even or odd using ternary operator.
+#include <stdio.h>
+
+int main(){
+    int num;
+    printf("Enter a number: ");
+    scanf("%d",&num);
+
+    num % 2 == 0 ? printf("The number is even.\n") : printf("The number is odd.\n");
+    return 0;
+}
