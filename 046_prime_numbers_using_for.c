35170873ebf5ea6b3a8142ca18122cca77feaab6
Saad Ahmed <saadahmd7223@gmail.com>
2025-08-19 23:56:13 +0530

    46/100

---
+++ 046_prime_numbers_using_for.c
@@ -0,0 +1,36 @@
+
+#include <stdio.h>
+
+int main()
+{
+    int num, isPrime = 1;
+    do
+    {
+        printf("Enter a number to check whether it is prime or not: ");
+        scanf("%d", &num);
+
+        if (num < 2)
+        {
+            printf("Please enter a number greater than or equal to 2.\n");
+        }
+    } while (num < 2);
+
+    for (int i = 2; i * i <= num; i++)
+    {
+        if (num % i == 0)
+        {
+            isPrime = 0;
+            break;
+        }
+    }
+
+if (isPrime == 1)
+{
+    printf("%d is a prime number \n", num);
+}
+else
+{
+    printf("%d is not a prime number\n", num);
+}
+return 0;
+}
