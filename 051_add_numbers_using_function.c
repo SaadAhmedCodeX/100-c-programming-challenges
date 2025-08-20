d885a44a50196590266985770799d72aa066c5a7
Saad Ahmed <saadahmd7223@gmail.com>
2025-08-21 00:16:49 +0530

    51/100

--- 051_add_numbers_using_function.c
+++ 051_add_numbers_using_function.c
@@ -1,8 +1,20 @@
 // Write a function that adds that takes 4,int parameters and returns the sum.
 #include <stdio.h>
-int add(int a, int b, int c, int d);
+int add(int, int , int , int );
+
 int main(){
-    
-    
+    int first, second, third, fourth, result;
+
+    printf("Enter four integers separated by spaces: ");
+    scanf("%d %d %d %d", &first, &second, &third, &fourth);
+
+    result = add(first, second, third, fourth);
+    printf("The sum of given numbers is %d\n", result);
     return 0;
+}
+
+int add(int a, int b, int c, int d){
+    int sum;
+    sum = a + b + c + d;
+    return sum;
 }
