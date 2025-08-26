d05fb0d61e490d0e9948934355c5ee410f7dd243
Saad Ahmed <saadahmd7223@gmail.com>
2025-08-27 00:09:27 +0530

    62/100

---
+++ 062_minmax_pointer.c
@@ -0,0 +1,31 @@
+// Implement a void minmax(int *a, int *b, int *min, int *max) function that takes two integer pointers a and b as input
+// and assigns the smaller value to min and the larger value to max using call by reference. Write a main function to test it with different values.
+#include <stdio.h>
+
+void min_max(int *, int *, int *, int *);
+
+int main()
+{
+    int first, second, min, max;
+
+    printf("Enter the first number: ");
+    scanf("%d", &first);
+    printf("Enter the second number: ");
+    scanf("%d", &second);
+
+    min_max(&first, &second, &min, &max);
+    printf("Between %d and %d, minimum is %d and maximum is %d", first, second, min, max);
+
+    return 0;
+}
+
+void min_max(int *a, int *b, int *min, int *max)
+{
+    if (*a < *b){
+        *min = *a;
+        *max = *b;
+    } else {
+        *min = *b;
+        *max = *a;
+    }
+}
