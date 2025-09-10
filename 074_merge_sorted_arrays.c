ccb29f8a919015435225034e269a595c6111d6eb
Saad Ahmed <saadahmd7223@gmail.com>
2025-09-11 00:14:41 +0530

    74/100

--- 074_merge_sorted_arrays.c
+++ 074_merge_sorted_arrays.c
@@ -1,28 +1,38 @@
 // Create a program to merge two sorted arrays.
 #include <stdio.h>
+#include <stdlib.h> 
 
 void input_arr(int arr[], int size);
-void merge_sorted_arrays(int arr1[], int arr2[], int arr3[], int size1, int size2, int size3);
+void merge_sorted_arrays(int arr1[], int arr2[], int arr3[], int size1, int size2);
 void display_arr(int arr[], int size);
 
-int main(){
+int main()
+{
     int size1, size2;
 
     printf("Enter number of elements for first array: ");
     scanf("%d", &size1);
-    int arr1[size1];
 
     printf("Enter number of elements for second array: ");
     scanf("%d", &size2);
-    int arr2[size2];
 
-    printf("Enter %d elements for first array: \n");
+    int *arr1 = malloc(size1 * sizeof(int));
+    int *arr2 = malloc(size2 * sizeof(int));
+    int *arr3 = malloc((size1 + size2) * sizeof(int));
+
+    if (arr1 == NULL || arr2 == NULL || arr3 == NULL)
+    {
+        printf("Memory allocation failed!\n");
+        return 1;
+    }
+
+    // This program works properly only if both the arrays entered are already sorted
+
+    printf("Enter %d elements for first array(sorted array):\n", size1);
     input_arr(arr1, size1);
-    printf("Enter %d elements for second array: \n");
-    input_arr(arr2, size2);
 
-    int size3 = size1 + size2;
-    int arr3[size3];
+    printf("Enter %d elements for second array(sorted array):\n", size2);
+    input_arr(arr2, size2);
 
     printf("Entered arrays:\n");
     printf("First array: ");
@@ -30,40 +40,51 @@ int main(){
     printf("Second array: ");
     display_arr(arr2, size2);
 
+    merge_sorted_arrays(arr1, arr2, arr3, size1, size2);
+
     printf("The merged array is: ");
-    merge_sorted_arrays(arr1, arr2, arr3, size1, size2, size3);
-    display_arr(arr3, size3);
+    display_arr(arr3, size1 + size2);
+
+    free(arr1);
+    free(arr2);
+    free(arr3);
 
     return 0;
 }
 
-void input_arr(int arr[], int size){
+void input_arr(int arr[], int size)
+{
     for (int i = 0; i < size; i++)
     {
         scanf("%d", &arr[i]);
     }
-    
 }
 
-void merge_sorted_arrays(int arr1[], int arr2[], int arr3[], int size1, int size2, int size3){
+void merge_sorted_arrays(int arr1[], int arr2[], int arr3[], int size1, int size2)
+{
     int i = 0, j = 0, k = 0;
-    while(k < size3){
-        if(j == size2){
-            arr3[k] = arr1[i];
-            i++;
-        } else if (i == size1){
-            arr3[k] = arr1[j];
-            j++;
-        } else if (arr1[i] < arr2[j]){
-            arr3[k] = arr1[i];
-            i++;
-        } else {
-            arr3[k] = arr2[j];
-            j++;
+
+    while (i < size1 && j < size2)
+    {
+        if (arr1[i] < arr2[j])
+        {
+            arr3[k++] = arr1[i++];
         }
-        k++;
+        else
+        {
+            arr3[k++] = arr2[j++];
+        }
     }
 
+    while (i < size1)
+    {
+        arr3[k++] = arr1[i++];
+    }
+
+    while (j < size2)
+    {
+        arr3[k++] = arr2[j++];
+    }
 }
 
 void display_arr(int arr[], int size)
