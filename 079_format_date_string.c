84ce797e9653c57c3ed60d0f5295e6e115887861
Saad Ahmed <saadahmd7223@gmail.com>
2025-09-20 00:01:11 +0530

    79/100

--- 079_format_date_string.c
+++ 079_format_date_string.c
@@ -1,19 +1,21 @@
 // Use printf with format specifiers to format and print a date string (day, month, year).
 #include <stdio.h>
 
-int main(){
+int main() {
     char day[10];
     char month[15];
     int year;
 
     printf("Enter the current day: ");
     scanf("%s", day);
+
     printf("Enter the current month: ");
     scanf("%s", month);
-    printf("Enter the current year: ");
-    scanf("%d", &day);
 
-    printf("The current date is: %s:%s:%04d", day, month, year);
+    printf("Enter the current year: ");
+    scanf("%d", &year);   
+
+    printf("The current date is: %s/%s/%04d\n", day, month, year);
 
     return 0;
 }
