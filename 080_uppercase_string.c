99f05802f884226f68daec12fe638ef3fd9d2c03
Saad Ahmed <saadahmd7223@gmail.com>
2025-09-21 23:58:12 +0530

    80/100

--- 080_uppercase_string.c
+++ 080_uppercase_string.c
@@ -1,6 +1,7 @@
 // Write a program to convert a input string to uppercase.
 #include <stdio.h>
 #include<ctype.h>
+#include<string.h>
 
 int main(){
     char text[100];
@@ -8,6 +9,7 @@ int main(){
     fgets(text, sizeof(text), stdin);
 
     for (int i = 0; text[i] != '\0'; i++)
+    for (int i = 0; i < strlen(text); i++)
     {
         text[i] = toupper(text[i]);
     }
