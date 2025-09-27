ca0aecc93b1222959756672d43a08145245cb6ba
Saad Ahmed <saadahmd7223@gmail.com>
2025-09-27 23:56:57 +0530

    84/100

---
+++ 084_trim_spaces.c
@@ -0,0 +1,8 @@
+// Implement a trim function that removes leading and trailing spaces from a string.
+#include <stdio.h>
+
+int main(){
+    char input[100];
+    char trimmed[100];
+    return 0;
+}
