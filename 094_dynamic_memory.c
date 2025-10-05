d223e68cec41903b705041cccc4ed5d2d675db92
Saad Ahmed <saadahmd7223@gmail.com>
2025-10-05 23:37:08 +0530

    94/100

--- 094_dynamic_memory.c
+++ 094_dynamic_memory.c
@@ -2,7 +2,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct Car {
+typedef struct Car
+{
     char company[50];
     char model[50];
     int year;
@@ -11,33 +12,36 @@ typedef struct Car {
 
 void printCarDescription(Car *c);
 
-int main() {
-    Car *my_car = (Car*) malloc(sizeof(Car)); // Memory allocation
+int main()
+{
+    Car *my_car = (Car *)malloc(sizeof(Car)); // Memory allocation
 
-   if (my_car == NULL) {
-    printf("Memory allocation failed!\n");
-    return 1;  // non-zero for error
-}
+    if (my_car == NULL)
+    {
+        printf("Memory allocation failed!\n");
+        return 1; // non-zero for error
+    }
     printf("Enter your car company: ");
-    scanf("%s", my_car-> company);
+    scanf("%s", my_car->company);
 
     printf("Enter your car model: ");
-    scanf("%s", my_car-> model);
+    scanf("%s", my_car->model);
 
     printf("Enter your car's manufacturing year: ");
-    scanf("%d", &my_car-> year);
+    scanf("%d", &my_car->year);
 
     printf("Enter your car color: ");
-    scanf("%s", my_car-> color);
+    scanf("%s", my_car->color);
 
     printf("Your car details:\n");
     printCarDescription(my_car);
 
-    free(my_car); // free
+    free(my_car); // Memory deallocation
     my_car = NULL;
     return 0;
 }
 
-void printCarDescription(Car *c){
+void printCarDescription(Car *c)
+{
     printf("The car is a %d %s %s in %s color.\n", c->year, c->company, c->model, c->color);
 }
