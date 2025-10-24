68cec47737bb5656b3477c648aade68646818699
Saad Ahmed <saadahmd7223@gmail.com>
2025-10-25 00:14:43 +0530

    104/100

--- 104_read_and_write_file.c
+++ 104_read_and_write_file.c
@@ -1,1 +1,25 @@
 // Create a program that performs both reading and writing operations on a file called data.txt
+#include <stdio.h>
+
+int main(){
+    FILE *file = fopen("104_data.txt", "r+");
+    
+    if(!file){
+        printf("Error in opening file\n");
+        return 1;
+    }
+
+    char buffer[100];
+
+    fgets(buffer, 100, file);
+    printf("Reading from file: %s\n", buffer);
+
+    fseek(file, 0, SEEK_END); // Moves the write cursor to the end
+
+    fputs("This message is written into the program\n", file);
+
+    fclose(file);
+    file = NULL;
+
+    return 0;
+}
